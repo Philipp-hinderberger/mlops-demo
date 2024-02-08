447d8c030cca315761a10d27ba31c96b540a2f37
philipp-hinderberger <hinderberger.philipp@gmail.com>
2024-02-08 14:19:12 +0100

    fixes

--- src/model/train.py
+++ src/model/train.py
@@ -25,12 +25,16 @@ def main(args):
 
 def get_csvs_df(path):
     if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+        raise RuntimeError(
+            f"Cannot use non-existent path provided: {path}"
+        )
 
     if os.path.isdir(path):
         csv_files = glob.glob(f"{path}/*.csv")
         if not csv_files:
-            raise RuntimeError(f"No CSV files found in provided data path: {path}")
+            raise RuntimeError(
+                f"No CSV files found in provided data path: {path}"
+            )
 
         return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
     else:
